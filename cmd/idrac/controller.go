7547a5e62f326cc06275c83c39fc11e76c7b033c
Alochym <alochym01@gmail.com>
2021-05-14 11:09:32 +0700

    finish opentelemetry with jaeger exporter

--- cmd/idrac/controller.go
+++ cmd/idrac/controller.go
@@ -6,22 +6,21 @@ import (
 	"net/http"
 
 	"go.opentelemetry.io/contrib/instrumentation/net/http/otelhttp"
-
 	"go.opentelemetry.io/otel"
-	"go.opentelemetry.io/otel/attribute"
-	"go.opentelemetry.io/otel/baggage"
-	"go.opentelemetry.io/otel/exporters/stdout"
+	"go.opentelemetry.io/otel/exporters/trace/jaeger"
 	"go.opentelemetry.io/otel/propagation"
 	"go.opentelemetry.io/otel/sdk/resource"
 	sdktrace "go.opentelemetry.io/otel/sdk/trace"
 	"go.opentelemetry.io/otel/semconv"
-	"go.opentelemetry.io/otel/trace"
 )
 
 func initTracer() {
-	// Create stdout exporter to be able to retrieve
+	// Create jaeger exporter to be able to retrieve
 	// the collected spans.
-	exporter, err := stdout.NewExporter(stdout.WithPrettyPrint())
+	jExporter, err := jaeger.NewRawExporter(
+		jaeger.WithCollectorEndpoint(jaeger.WithEndpoint("http://localhost:14268/api/traces")),
+	)
+
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -29,9 +28,9 @@ func initTracer() {
 	// For the demonstration, use sdktrace.AlwaysSample sampler to sample all traces.
 	// In a production application, use sdktrace.ProbabilitySampler with a desired probability.
 	tp := sdktrace.NewTracerProvider(
-		sdktrace.WithSampler(sdktrace.AlwaysSample()),
-		sdktrace.WithSyncer(exporter),
-		sdktrace.WithResource(resource.NewWithAttributes(semconv.ServiceNameKey.String("ExampleService"))),
+		// set Span Name
+		sdktrace.WithResource(resource.NewWithAttributes(semconv.ServiceNameKey.String("Alochym"))),
+		sdktrace.WithSyncer(jExporter),
 	)
 	if err != nil {
 		log.Fatal(err)
@@ -43,22 +42,29 @@ func initTracer() {
 func main() {
 	initTracer()
 
-	uk := attribute.Key("username")
-
-	helloHandler := func(w http.ResponseWriter, req *http.Request) {
-		ctx := req.Context()
-		span := trace.SpanFromContext(ctx)
-		username := baggage.Value(ctx, uk)
-		span.AddEvent("handling this...", trace.WithAttributes(uk.String(username.AsString())))
-
-		_, _ = io.WriteString(w, "Hello, world!\n")
-	}
-
 	otelHandler := otelhttp.NewHandler(http.HandlerFunc(helloHandler), "Hello")
 
-	http.Handle("/hello", otelHandler)
+	// http.HandleFunc("/hello", helloHandler)
+	http.Handle	("/hello", otelHandler)
 	err := http.ListenAndServe(":8080", nil)
 	if err != nil {
 		panic(err)
 	}
+}
+
+func helloHandler(w http.ResponseWriter, req *http.Request) {
+	// get global Trace Provider
+	tr := otel.GetTracerProvider().Tracer("Alochym")
+
+	// get context from http request
+	ctx := req.Context()
+
+	// start tracing
+	_, sp := tr.Start(ctx, "helloHandler")
+
+	// close tracing
+	defer sp.End()
+
+	// http response
+	_, _ = io.WriteString(w, "Hello, world!\n")
 }
