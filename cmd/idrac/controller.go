64889a1eb9ee1024fd1d25fd38f215500be202f6
Alochym <alochym01@gmail.com>
2021-05-17 08:26:02 +0700

    finish opentelemetry tracing

--- cmd/idrac/controller.go
+++ cmd/idrac/controller.go
@@ -1,12 +1,15 @@
 package main
 
 import (
+	"context"
+	"fmt"
 	"io"
 	"log"
 	"net/http"
 
-	"go.opentelemetry.io/contrib/instrumentation/net/http/otelhttp"
+	// "go.opentelemetry.io/contrib/instrumentation/net/http/otelhttp"
 	"go.opentelemetry.io/otel"
+	"go.opentelemetry.io/otel/attribute"
 	"go.opentelemetry.io/otel/exporters/trace/jaeger"
 	"go.opentelemetry.io/otel/sdk/resource"
 	sdktrace "go.opentelemetry.io/otel/sdk/trace"
@@ -41,9 +44,10 @@ func initTracer() {
 func main() {
 	initTracer()
 
-	otelHandler := otelhttp.NewHandler(http.HandlerFunc(helloHandler), "Hello")
+	// otelHandler := otelhttp.NewHandler(http.HandlerFunc(helloHandler), "Hello")
 
-	http.Handle("/hello", otelHandler)
+	// http.Handle("/hello", otelHandler)
+	http.HandleFunc("/hello", helloHandler)
 	err := http.ListenAndServe(":8080", nil)
 	if err != nil {
 		panic(err)
@@ -58,11 +62,31 @@ func helloHandler(w http.ResponseWriter, req *http.Request) {
 	ctx := req.Context()
 
 	// start tracing
-	_, sp := tr.Start(ctx, "helloHandler")
+	span_ctx, sp := tr.Start(ctx, "helloHandler")
+	usingCtx(span_ctx)
 
 	// close tracing
 	defer sp.End()
 
 	// http response
 	_, _ = io.WriteString(w, "Hello, world!\n")
+}
+
+func usingCtx(ctx context.Context) {
+	tr := otel.GetTracerProvider().Tracer("Alochym")
+
+	// get context from http request
+	// ctx := .Context()
+
+	// start tracing
+	_, sp := tr.Start(ctx, "usingCtx")
+	sp.SetAttributes(
+		attribute.String("usingCtx Function", "OLACHYM"),
+		attribute.String("Function", "OLACHYM 02"),
+	)
+
+	fmt.Println("alochym")
+	// close tracing
+	defer sp.End()
+
 }
